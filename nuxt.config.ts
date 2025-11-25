a63d56699863c46e609260093850e9665b219441
LucyK12345 <sabrina@kmwe.de>
2025-11-25 20:51:58 +0100

    update: npm packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-07-15',
